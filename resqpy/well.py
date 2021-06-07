f80fd5abe20536522aa2f4fe93570f762ea81afd
emmanesbit <84792288+emmanesbit@users.noreply.github.com>
2021-06-07 13:46:06 +0100

    Update resqpy/well.py
    Co-authored-by: connortann <71127464+connortann@users.noreply.github.com>

--- resqpy/well.py
+++ resqpy/well.py
@@ -866,7 +866,7 @@ class Trajectory():
       md_datum_part = relatives_model.part_for_uuid(md_datum_uuid)
       assert md_datum_part, 'md datum part not found in model'
       self.md_datum = MdDatum(self.model, md_datum_root = relatives_model.root_for_part(md_datum_part))
-      ds_uuid = bu.uuid_from_string(rqet.find_tag_text(rqet.find_tag(node, 'DeviationSurvey'), 'UUID'))
+      ds_uuid = bu.uuid_from_string(rqet.find_nested_tags_text(node, ['DeviationSurvey', 'UUID']))
       if ds_uuid is not None:  # this will probably not work when relatives model is different from self.model
          ds_part = 'obj_DeviationSurveyRepresentation_' + str(ds_uuid) + '.xml'
          self.deviation_survey = DeviationSurvey(
