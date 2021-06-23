2dba85a6f6c571363fcea4ea5b8673869b4d91f6
Andy Beer <andy.being@gmail.com>
2021-06-23 12:04:32 +0200

    added test function for new Property class

--- tests/test_property.py
+++ tests/test_property.py
@@ -1,8 +1,56 @@
 import pytest
 
+import os
+import numpy as np
+
+import resqpy.model as rq
+import resqpy.grid as grr
 import resqpy.property as rqp
 import resqpy.olio.weights_and_measures as bwam
 
+
+# ---- Test PropertyCollection ---
+
+# TODO
+
+
+# ---- Test Property ---
+
+def test_property(tmp_path):
+   epc = os.path.join(tmp_path, 'test_prop.epc')
+   model = rq.new_model(epc)
+   grid = grr.RegularGrid(model, extent_kji = (2, 3, 4))
+   grid.write_hdf5()
+   grid.create_xml()
+   a1 = np.random.random(grid.extent_kji)
+   p1 = rqp.Property(model)
+   p1.from_array(a1, source_info = 'random', keyword = 'NETGRS', support_uuid = grid.uuid,
+                 property_kind = 'net to gross ratio', indexable_element = 'cells', uom = 'm3/m3')
+   pk = rqp.PropertyKind(model, title = 'facies', parent_property_kind = 'discrete')
+   pk.create_xml()
+   facies_dict = {0: 'background'}
+   for i in range(1, 10): facies_dict[i] = 'facies ' + str(i)
+   sl = rqp.StringLookup(model, int_to_str_dict = facies_dict, title = 'facies table')
+   sl.create_xml()
+   shape2 = tuple(list(grid.extent_kji).append(2))
+   a2 = (np.random.random(shape2) * 10.0).astype(int)
+   p2 = rqp.Property(model)
+   p2.from_array(a2, source_info = 'random', keyword = 'FACIES', support_uuid = grid.uuid,
+                 local_property_kind_uuid = pk.uuid, indexable_element = 'cells', discrete = True,
+                 null_value = 0, count = 2, string_lookup_uuid = sl.uuid)
+   model.store_epc()
+   model = rq.Model(epc)
+   ntg_uuid = model.uuid(obj_type = p1.resqml_type, title = 'NETGRS')
+   assert ntg_uuid is not None
+   p1p = rqp.Property(model, uuid = ntg_uuid)
+   assert np.all(p1p.array_ref() == p1.array_ref())
+   facies_uuid = model.uuid(obj_type = p2.resqml_type, title = 'FACIES')
+   assert facies_uuid is not None
+   p2p = rqp.Property(model, uuid = facies_uuid)
+   assert np.all(p2p.array_ref() == p2.array_ref())
+   grid = model.grid()
+   assert grid.property_collection.number_of_parts() == 2
+   
 
 # ---- Test uom from string ---
 
