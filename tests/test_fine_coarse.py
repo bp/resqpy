47c38cb86c03121883636a147d25f4e31756a294
Andy Beer <andy.being@gmail.com>
2021-07-13 11:29:08 +0200

    adding test of tartan refinement fine_coarse function

--- tests/test_fine_coarse.py
+++ tests/test_fine_coarse.py
@@ -34,8 +34,27 @@ def test_fine_coarse():
 
 
 def test_fine_coarse_functions():
+
    assert rqfc.axis_for_letter('k') == 0
    assert rqfc.axis_for_letter('J') == 1
    assert rqfc.axis_for_letter('i') == 2
    assert rqfc.letter_for_axis(0) == 'K'
    assert rqfc.letter_for_axis(2) == 'I'
+
+   fct = rqfc.tartan_refinement(
+      coarse_extent_kji = (5, 7, 7),
+      coarse_fovea_box = np.array([[0, 2, 2], [1, 4, 4]], dtype = int),
+      fovea_ratios_kji = (3, 3, 4),
+      decay_rates_kji = (1, 1, 1),
+      decay_mode = 'linear')
+   assert fct is not None
+   fct.assert_valid()
+
+   assert fct.coarse_extent_kji == (5, 7, 7)
+   assert fct.fine_extent_kji == (10, 15, 22)
+   assert np.all(fct.coarse_for_fine_axial_vector(0) == (0, 0, 0, 1, 1, 1, 2, 2, 3, 4))
+   for const in fct.contant_ratios: assert const is None
+   assert np.all(fct.vector_ratios[1] == (1, 2, 3, 3, 3, 2, 1))
+   assert np.all(fct.vector_ratios[2] == (2, 3, 4, 4, 4, 3, 2))
+
+   # todo: add test of tartan refinement in exponential mode
