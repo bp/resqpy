5ae90b6ca2670ce64cb447f51f6d5e7445141902
Andy Beer <andy.beer@bp.com>
2021-08-30 08:39:26 +0300

    removing erroneous local file name in unstructured test

--- tests/test_unstructured.py
+++ tests/test_unstructured.py
@@ -9,8 +9,8 @@ import resqpy.unstructured as rqu
 
 def test_hexa_grid_from_grid(example_model_with_properties):
 
-   #   model = example_model_with_properties
-   model = rq.Model('/users/andy/bifröst/bc/u_test.epc', copy_from = example_model_with_properties.epc_file)
+   model = example_model_with_properties
+   # model = rq.Model('/users/andy/bifröst/bc/u_test.epc', copy_from = example_model_with_properties.epc_file)
 
    ijk_grid_uuid = model.uuid(obj_type = 'IjkGridRepresentation')
    assert ijk_grid_uuid is not None
