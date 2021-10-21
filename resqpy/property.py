495bb91099f6015aa4eef5c7b171cd7c7abd61dc
Andy Beer <andy.beer@bp.com>
2021-10-21 12:12:28 +0100

    version date update

--- resqpy/property.py
+++ resqpy/property.py
@@ -1,6 +1,6 @@
 """property.py: module handling collections of RESQML properties for grids, wellbore frames, grid connection sets etc."""
 
-version = '20th October 2021'
+version = '21st October 2021'
 
 # Nexus is a registered trademark of the Halliburton Company
 
