1164682686445604d64407598ec2b3ae3f4f7cf8
Nirjhor Chakraborty <nirjhor.chakraborty@bp.com>
2021-11-15 10:46:04 -0600

    rewiring imports

--- tests/test_resqpy.py
+++ tests/test_resqpy.py
@@ -7,5 +7,12 @@ def test_empty_model():
 
 
 def test_all_imports():
+    from resqpy import (crs, derived_model, fault, grid, grid_surface, lines, organize, property, rq_import, surface,
+                        time_series, well)
+
+    # The line below prevents IDEs from deleting the above
+    _ = (crs, derived_model, fault, grid, grid_surface, lines, organize, property, rq_import, surface,
+                        time_series, well)
+
     #    from resqpy.olio import *
     return
