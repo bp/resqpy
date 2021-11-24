188443f2229c6a861719a9243ab7429d0c0fa21a
Nirjhor Chakraborty <nirjhor.chakraborty@bp.com>
2021-11-23 18:24:35 -0600

    collecting imports for completeness

--- resqpy/organize/__init__.py
+++ resqpy/organize/__init__.py
@@ -1,25 +1,34 @@
 __all__ = [
     'OrganizationFeature',
+    'GeobodyFeature',
+    'BoundaryFeature',
+    'FrontierFeature',
+    'FluidBoundaryFeature',
+    'RockFluidUnitFeature',
+    'TectonicBoundaryFeature',
+    'GeneticBoundaryFeature',
+    'FluidBoundaryFeature',
+    'RockFluidUnitFeature',
+    'TectonicBoundaryFeature',
+    'GeneticBoundaryFeature',
 ]
-
-from .organization_feature import OrganizationFeature
-from .geobody_feature import GeobodyFeature
-from .boundary_feature import BoundaryFeature
-
-from .frontier_feature import FrontierFeature
-from .geologic_unit_feature import GeologicUnitFeature
-from .fluid_boundary_feature import FluidBoundaryFeature
-from .rock_fluid_unit_feature import RockFluidUnitFeature
-from .tectonic_boundary_feature import TectonicBoundaryFeature
-from .genetic_boundary_feature import GeneticBoundaryFeature
-from .wellbore_feature import WellboreFeature
-from .fault_interpretation import FaultInterpretation
-from .earth_model_interpretation import EarthModelInterpretation
-from .horizon_interpretation import HorizonInterpretation
-from .geobody_boundary_interpretation import GeobodyBoundaryInterpretation
-from .geobody_interpretation import GeobodyInterpretation
-from .wellbore_interpretation import WellboreInterpretation
 
 from resqpy.organize.organize_functions import (equivalent_extra_metadata, alias_for_attribute,
                                                 extract_has_occurred_during, equivalent_chrono_pairs,
                                                 create_xml_has_occurred_during)
+from .boundary_feature import BoundaryFeature
+from .earth_model_interpretation import EarthModelInterpretation
+from .fault_interpretation import FaultInterpretation
+from .fluid_boundary_feature import FluidBoundaryFeature
+from .frontier_feature import FrontierFeature
+from .genetic_boundary_feature import GeneticBoundaryFeature
+from .geobody_boundary_interpretation import GeobodyBoundaryInterpretation
+from .geobody_feature import GeobodyFeature
+from .geobody_interpretation import GeobodyInterpretation
+from .geologic_unit_feature import GeologicUnitFeature
+from .horizon_interpretation import HorizonInterpretation
+from .organization_feature import OrganizationFeature
+from .rock_fluid_unit_feature import RockFluidUnitFeature
+from .tectonic_boundary_feature import TectonicBoundaryFeature
+from .wellbore_feature import WellboreFeature
+from .wellbore_interpretation import WellboreInterpretation
