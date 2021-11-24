e5cb6966a42638810dca936eca3e95ed30a4dd76
Andy Beer <andy.beer@bp.com>
2021-11-24 17:28:11 +0000

    docstring added to package init for lines

--- resqpy/lines/__init__.py
+++ resqpy/lines/__init__.py
@@ -1,3 +1,5 @@
+"""Package containing Polyline and PolylineSet classes."""
+
 __all__ = ['Polyline', 'PolylineSet', 'load_hdf5_array', 'shift_polyline', 'flatten_polyline', 'tangents', 'spline']
 
 from .common import load_hdf5_array, shift_polyline, flatten_polyline, tangents, spline
