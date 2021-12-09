5b0e07f23638b488493c3e70a14c57c099e72c3f
Emma Nesbit <emma.nesbit@uk.bp.com>
2021-12-09 08:33:57 +0000

    Updated init, extra docstrings, moving GridFromCp class to within _grid_from_cp submodule.

--- resqpy/rq_import/__init__.py
+++ resqpy/rq_import/__init__.py
@@ -1,11 +1,9 @@
 """Functions for RESQML objects related to importing"""
 
-all = [
-    '_import_nexus.py', '_GridFromCp.py', '_import_vdb_all_grids.py', '_grid_from_cp.py', '_import_vdb_ensemble.py',
-    '_add_ab_properties.py', '_add_surfaces.py'
+__all__ = [
+    'import_nexus', 'import_vdb_all_grids', 'grid_from_cp', 'import_vdb_ensemble', 'add_ab_properties', 'add_surfaces'
 ]
 
-from ._GridFromCp import GridFromCp
 from ._grid_from_cp import grid_from_cp
 from ._import_nexus import import_nexus
 from ._import_vdb_all_grids import import_vdb_all_grids
