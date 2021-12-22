5552097cc0ab57e12f0b39c5bac3b92e822a2db4
Chris Flynn <Chris@Flynnsoftware.com>
2021-12-22 11:13:22 +0000

    Fixing the module level declaration for a moved function.

--- resqpy/grid/__init__.py
+++ resqpy/grid/__init__.py
@@ -9,5 +9,6 @@ from .transmissibility import transmissibility
 from .grid import Grid
 from .regular_grid import RegularGrid
 from .grid_types import grid_flavour, is_regular_grid, any_grid
-from .moved_functions import find_cell_for_x_sect_xz, establish_zone_property_kind
+from .moved_functions import establish_zone_property_kind
 from .extract_functions import extract_grid_parent, extent_kji_from_root
+from .points_functions import find_cell_for_x_sect_xz
--- resqpy/grid/moved_functions.py
+++ resqpy/grid/moved_functions.py
@@ -13,25 +13,3 @@ def establish_zone_property_kind(model):
         'location.', DeprecationWarning)
 
     return pk.establish_zone_property_kind(model)
-
-
-def find_cell_for_x_sect_xz(x_sect, x, z):
-    """MOVED: Returns the (k0, j0) or (k0, i0) indices of the cell containing point x,z in the cross section.
-
-    arguments:
-       x_sect (numpy float array of shape (nk, nj or ni, 2, 2, 2 or 3): the cross section x,z or x,y,z data
-       x (float) x-coordinate of point of interest in the cross section space
-       z (float): y-coordinate of  point of interest in the cross section space
-
-    note:
-       the x_sect data is in the form returned by x_section_corner_points() or split_gap_x_section_points();
-       the 2nd of the returned pair is either a J index or I index, whichever was not the axis specified
-       when generating the x_sect data; returns (None, None) if point inclusion not detected; if xyz data is
-       provided, the y values are ignored; note that the point of interest x,z coordinates are in the space of
-       x_sect, so if rotation has occurred, the x value is no longer an easting and is typically picked off a
-       cross section plot
-    """
-    warnings.warn('This function has been moved to grid/points_functions. Please update your code to the new location.',
-                  DeprecationWarning)
-
-    return pf.find_cell_for_x_sect_xz(x_sect, x, z)
