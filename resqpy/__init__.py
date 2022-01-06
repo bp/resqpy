a16efc51454adad356610905241d105bc43ad3cd
Connor Tann <71127464+connortann@users.noreply.github.com>
2022-01-06 12:22:18 +0000

    Ensure dev version works when not in root dir

--- resqpy/__init__.py
+++ resqpy/__init__.py
@@ -36,7 +36,7 @@ except ImportError:
     # Dev setup: Dynamically get version from local git history
     try:
         from setuptools_scm import get_version
-        __version__ = get_version()
+        __version__ = get_version(root = '..', relative_to = __file__)
     except Exception:
         __version__ = "0.0.0-version-not-available"
 except Exception:
