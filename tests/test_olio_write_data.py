8257286e06f961965c8da408c1e452942896597d
Max Maunder <max.maunder@bjss.com>
2022-01-10 13:06:04 +0000

    Tests reformatted

--- tests/test_olio_write_data.py
+++ tests/test_olio_write_data.py
@@ -6,15 +6,13 @@ from pytest_mock import MockerFixture
 
 def test_write_pure_binary_data(mocker: MockerFixture, caplog):
     # Arrange
-    test_array = np.array([[[0, 0], [0,  0]], [[0, 0], [0, 0]]])
+    test_array = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
     open_mock = mocker.mock_open()
     fileno_mock = mocker.Mock(return_value = 1, name = 'fileno_mock')
     open_mock.return_value.fileno = fileno_mock
 
     mocker.patch("builtins.open", open_mock)
-    expected_calls = [
-        mocker.call('test', 'wb'),
-        mocker.call().__enter__()]
+    expected_calls = [mocker.call('test', 'wb'), mocker.call().__enter__()]
 
     # Act
     wd.write_pure_binary_data('test', test_array)
@@ -26,7 +24,7 @@ def test_write_pure_binary_data(mocker: MockerFixture, caplog):
 def test_write_array_to_ascii_file_decimals_equal_0(mocker: MockerFixture):
     # Arrange
     test_extent = np.array([2, 2, 2])
-    test_array = np.array([[[0, 0], [0,  0]], [[0, 0], [0, 0]]])
+    test_array = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
     open_mock = mocker.mock_open()
     mocker.patch("builtins.open", open_mock)
     expected_calls = [
@@ -55,10 +53,11 @@ def test_write_array_to_ascii_file_decimals_equal_0(mocker: MockerFixture):
         mocker.call().write('\t'),
         mocker.call().write('0'),
         mocker.call().write('\n'),
-        mocker.call().__exit__(None, None, None)]
+        mocker.call().__exit__(None, None, None)
+    ]
 
     # Act
-    wd.write_array_to_ascii_file('test', test_extent, test_array, decimals=0)
+    wd.write_array_to_ascii_file('test', test_extent, test_array, decimals = 0)
 
     # Assert
     open_mock.assert_has_calls(expected_calls)
@@ -67,18 +66,16 @@ def test_write_array_to_ascii_file_decimals_equal_0(mocker: MockerFixture):
 def test_write_array_to_ascii_file_binary(mocker: MockerFixture, caplog):
     # Arrange
     test_extent = np.array([2, 2, 2])
-    test_array = np.array([[[0, 0], [0,  0]], [[0, 0], [0, 0]]])
+    test_array = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
     open_mock = mocker.mock_open()
     fileno_mock = mocker.Mock(return_value = 1, name = 'fileno_mock')
     open_mock.return_value.fileno = fileno_mock
 
     mocker.patch("builtins.open", open_mock)
-    expected_calls = [
-        mocker.call('test.db', 'wb'),
-        mocker.call().__enter__()]
+    expected_calls = [mocker.call('test.db', 'wb'), mocker.call().__enter__()]
 
     # Act
-    wd.write_array_to_ascii_file('test', test_extent, test_array, use_binary=True, binary_only=True)
+    wd.write_array_to_ascii_file('test', test_extent, test_array, use_binary = True, binary_only = True)
 
     # Assert
     open_mock.assert_has_calls(expected_calls)
@@ -88,16 +85,17 @@ def test_write_array_to_ascii_file_binary(mocker: MockerFixture, caplog):
 def test_write_array_to_ascii_file_no_headers(mocker: MockerFixture):
     # Arrange
     test_extent = np.array([2, 2, 2])
-    test_array = np.array([[[0, 0], [0,  0]], [[0, 0], [0, 0]]])
+    test_array = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
     open_mock = mocker.mock_open()
     mocker.patch("builtins.open", open_mock)
     unexpected_calls = [
         mocker.call().write('! Data written by write_array_to_ascii_file() python function\n'),
         mocker.call().write('! Extent of array is: [2, 2, 2]\n'),
-        mocker.call().write('! Maximum 20 data items per line\n')]
+        mocker.call().write('! Maximum 20 data items per line\n')
+    ]
 
     # Act
-    wd.write_array_to_ascii_file('test', test_extent, test_array, headers=False)
+    wd.write_array_to_ascii_file('test', test_extent, test_array, headers = False)
 
     # Assert
     assert unexpected_calls not in open_mock.mock_calls
@@ -106,7 +104,7 @@ def test_write_array_to_ascii_file_no_headers(mocker: MockerFixture):
 def test_write_array_to_ascii_file_bool(mocker: MockerFixture):
     # Arrange
     test_extent = np.array([2, 2, 2])
-    test_array = np.array([[[0, 1], [0,  0]], [[1, 0], [1, 0]]])
+    test_array = np.array([[[0, 1], [0, 0]], [[1, 0], [1, 0]]])
     open_mock = mocker.mock_open()
     mocker.patch("builtins.open", open_mock)
     expected_calls = [
@@ -135,10 +133,11 @@ def test_write_array_to_ascii_file_bool(mocker: MockerFixture):
         mocker.call().write('\t'),
         mocker.call().write('0'),
         mocker.call().write('\n'),
-        mocker.call().__exit__(None, None, None)]
+        mocker.call().__exit__(None, None, None)
+    ]
 
     # Act
-    wd.write_array_to_ascii_file('test', test_extent, test_array, data_type='bool')
+    wd.write_array_to_ascii_file('test', test_extent, test_array, data_type = 'bool')
 
     # Assert
     open_mock.assert_has_calls(expected_calls)
@@ -147,11 +146,10 @@ def test_write_array_to_ascii_file_bool(mocker: MockerFixture):
 def test_write_array_to_ascii_file_append(mocker: MockerFixture):
     # Arrange
     test_extent = np.array([2, 2, 2])
-    test_array = np.array([[[0, 0], [0,  0]], [[0, 0], [0, 0]]])
+    test_array = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
     open_mock = mocker.mock_open()
     mocker.patch("builtins.open", open_mock)
-    expected_calls = [
-        mocker.call('test', 'a')]
+    expected_calls = [mocker.call('test', 'a')]
 
     # Act
     wd.write_array_to_ascii_file('test', test_extent, test_array, append = True)
@@ -163,7 +161,7 @@ def test_write_array_to_ascii_file_append(mocker: MockerFixture):
 def test_write_array_to_ascii_file_space_separated(mocker: MockerFixture):
     # Arrange
     test_extent = np.array([2, 2, 2])
-    test_array = np.array([[[0, 0], [0,  0]], [[0, 0], [0, 0]]])
+    test_array = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
     open_mock = mocker.mock_open()
     mocker.patch("builtins.open", open_mock)
     expected_calls = [
@@ -192,7 +190,8 @@ def test_write_array_to_ascii_file_space_separated(mocker: MockerFixture):
         mocker.call().write(' '),
         mocker.call().write('0.000'),
         mocker.call().write('\n'),
-        mocker.call().__exit__(None, None, None)]
+        mocker.call().__exit__(None, None, None)
+    ]
 
     # Act
     wd.write_array_to_ascii_file('test', test_extent, test_array, space_separated = True)
@@ -204,7 +203,7 @@ def test_write_array_to_ascii_file_space_separated(mocker: MockerFixture):
 def test_write_array_to_ascii_file_nan_substitute(mocker: MockerFixture):
     # Arrange
     test_extent = np.array([2, 2, 2])
-    test_array = np.array([[[1, np.nan], [3,  2]], [[1, 5], [np.nan, np.nan]]])
+    test_array = np.array([[[1, np.nan], [3, 2]], [[1, 5], [np.nan, np.nan]]])
     open_mock = mocker.mock_open()
     mocker.patch("builtins.open", open_mock)
     expected_calls = [
@@ -233,7 +232,8 @@ def test_write_array_to_ascii_file_nan_substitute(mocker: MockerFixture):
         mocker.call().write('\t'),
         mocker.call().write('0.000'),
         mocker.call().write('\n'),
-        mocker.call().__exit__(None, None, None)]
+        mocker.call().__exit__(None, None, None)
+    ]
 
     # Act
     wd.write_array_to_ascii_file('test', test_extent, test_array, nan_substitute_value = 0)
@@ -245,7 +245,7 @@ def test_write_array_to_ascii_file_nan_substitute(mocker: MockerFixture):
 def test_write_array_to_ascii_file_blank_line_after_j_block(mocker: MockerFixture):
     # Arrange
     test_extent = np.array([2, 2, 2])
-    test_array = np.array([[[0, 0], [0,  0]], [[0, 0], [0, 0]]])
+    test_array = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
     open_mock = mocker.mock_open()
     mocker.patch("builtins.open", open_mock)
     expected_calls = [
@@ -276,7 +276,8 @@ def test_write_array_to_ascii_file_blank_line_after_j_block(mocker: MockerFixtur
         mocker.call().write('0.000'),
         mocker.call().write('\n'),
         mocker.call().write('\n'),
-        mocker.call().__exit__(None, None, None)]
+        mocker.call().__exit__(None, None, None)
+    ]
 
     # Act
     wd.write_array_to_ascii_file('test', test_extent, test_array, blank_line_after_j_block = True)
