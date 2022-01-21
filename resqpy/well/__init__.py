e609ae4757575fff5e4a083e7a8e31087600603c
Andy Beer <andy.beer@bp.com>
2022-01-21 16:47:52 +0000

    correcting well package init for docstring processing to readthedocs

--- resqpy/well/__init__.py
+++ resqpy/well/__init__.py
@@ -16,3 +16,9 @@ from ._wellbore_frame import WellboreFrame
 
 from .well_object_funcs import add_wells_from_ascii_file, well_name, add_las_to_trajectory, \
     add_blocked_wells_from_wellspec, add_logs_from_cellio, lookup_from_cellio
+
+# Set "module" attribute of all public objects to this path. Fixes issue #310
+for _name in __all__:
+    _obj = eval(_name)
+    if hasattr(_obj, "__module__"):
+        _obj.__module__ = __name__
