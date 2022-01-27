e4e2472936f94a4876de3e2cdf298ea6a37f395f
Max Maunder <max.maunder@bjss.com>
2022-01-27 09:29:21 +0000

    Tests added but expected values need to be checked

--- tests/unit_tests/grid/conftest.py
+++ tests/unit_tests/grid/conftest.py
@@ -19,11 +19,11 @@ def model_test(tmp_path) -> Model:
 
 @pytest.fixture
 def basic_regular_grid(model_test: Model) -> Grid:
-    return grr.RegularGrid(model_test, extent_kji = (2, 2, 2), dxyz = (100.0, 50.0, 20.0), as_irregular_grid = True)
+    return grr.RegularGrid(model_test, extent_kji=(2, 2, 2), dxyz=(100.0, 50.0, 20.0), as_irregular_grid=True)
 
 
 @pytest.fixture
-def example_model_with_properties(tmp_path):
+def example_model_with_properties(tmp_path) -> Model:
     """Model with a grid (5x5x3) and properties.
    Properties:
    - Zone (discrete)
@@ -35,29 +35,29 @@ def example_model_with_properties(tmp_path):
    - SW (continuous)
    """
     model_path = str(tmp_path / 'test_no_rels.epc')
-    model = Model(create_basics = True, create_hdf5_ext = True, epc_file = model_path, new_epc = True)
+    model = Model(create_basics=True, create_hdf5_ext=True, epc_file=model_path, new_epc=True)
     model.store_epc(model.epc_file)
 
-    grid = grr.RegularGrid(parent_model = model,
-                           origin = (0, 0, 0),
-                           extent_kji = (3, 5, 5),
-                           crs_uuid = rqet.uuid_for_part_root(model.crs_root),
-                           set_points_cached = True,
-                           as_irregular_grid = True,
-                           dxyz = (100.0, 50.0, 20.0))
+    grid = grr.RegularGrid(parent_model=model,
+                           origin=(0, 0, 0),
+                           extent_kji=(3, 5, 5),
+                           crs_uuid=rqet.uuid_for_part_root(model.crs_root),
+                           set_points_cached=True,
+                           as_irregular_grid=True,
+                           dxyz=(100.0, 50.0, 20.0))
     grid.cache_all_geometry_arrays()
-    grid.write_hdf5_from_caches(file = model.h5_file_name(file_must_exist = False), mode = 'w')
+    grid.write_hdf5_from_caches(file=model.h5_file_name(file_must_exist=False), mode='w')
 
-    grid.create_xml(ext_uuid = model.h5_uuid(),
-                    title = 'grid',
-                    write_geometry = True,
-                    add_cell_length_properties = False)
+    grid.create_xml(ext_uuid=model.h5_uuid(),
+                    title='grid',
+                    write_geometry=True,
+                    add_cell_length_properties=False)
     model.store_epc()
 
     model = Model(model_path)
 
-    zone = np.ones(shape = (5, 5))
-    zone_array = np.array([zone, zone + 1, zone + 2], dtype = 'int')
+    zone = np.ones(shape=(5, 5))
+    zone_array = np.array([zone, zone + 1, zone + 2], dtype='int')
 
     vpc = np.array([[1, 1, 1, 2, 2], [1, 1, 1, 2, 2], [1, 1, 1, 2, 2], [1, 1, 1, 2, 2], [1, 1, 1, 2, 2]])
     vpc_array = np.array([vpc, vpc, vpc])
@@ -82,30 +82,37 @@ def example_model_with_properties(tmp_path):
 
     perm = np.array([[1, 10, 10, 100, 100], [1, 10, 10, 100, 100], [1, 10, 10, 100, 100], [1, 10, 10, 100, 100],
                      [1, 10, 10, 100, 100]])
-    perm_array = np.array([perm, perm, perm], dtype = 'float')
+    perm_array = np.array([perm, perm, perm], dtype='float')
 
     collection = rqp.GridPropertyCollection()
     collection.set_grid(grid)
     for array, name, kind, discrete, facet_type, facet in zip(
-        [zone_array, vpc_array, fb_array, facies_array, ntg_array, por_array, sat_array, perm_array],
-        ['Zone', 'VPC', 'Fault block', 'Facies', 'NTG', 'POR', 'SW', 'Perm'], [
-            'discrete', 'discrete', 'discrete', 'discrete', 'net to gross ratio', 'porosity', 'saturation',
-            'permeability rock'
-        ], [True, True, True, True, False, False, False, False],
-        [None, None, None, None, None, None, None, 'direction'], [None, None, None, None, None, None, None, 'I']):
-        collection.add_cached_array_to_imported_list(cached_array = array,
-                                                     source_info = '',
-                                                     keyword = name,
-                                                     discrete = discrete,
-                                                     uom = None,
-                                                     time_index = None,
-                                                     null_value = None,
-                                                     property_kind = kind,
-                                                     facet_type = facet_type,
-                                                     facet = facet,
-                                                     realization = None)
+            [zone_array, vpc_array, fb_array, facies_array, ntg_array, por_array, sat_array, perm_array],
+            ['Zone', 'VPC', 'Fault block', 'Facies', 'NTG', 'POR', 'SW', 'Perm'], [
+                'discrete', 'discrete', 'discrete', 'discrete', 'net to gross ratio', 'porosity', 'saturation',
+                'permeability rock'
+            ], [True, True, True, True, False, False, False, False],
+            [None, None, None, None, None, None, None, 'direction'], [None, None, None, None, None, None, None, 'I']):
+        collection.add_cached_array_to_imported_list(cached_array=array,
+                                                     source_info='',
+                                                     keyword=name,
+                                                     discrete=discrete,
+                                                     uom=None,
+                                                     time_index=None,
+                                                     null_value=None,
+                                                     property_kind=kind,
+                                                     facet_type=facet_type,
+                                                     facet=facet,
+                                                     realization=None)
         collection.write_hdf5_for_imported_list()
         collection.create_xml_for_imported_list_and_add_parts_to_model()
     model.store_epc()
 
     return model
+
+
+@pytest.fixture()
+def faulted_grid() -> Grid:
+    epc_file = '../../test_data/wren/wren.epc'
+    model = Model(epc_file=epc_file)
+    return model.grid(title='faulted grid')
