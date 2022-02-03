024a27f565b259f66df3d0b9b4e4411ab126b1e6
Chris Flynn <Chris@Flynnsoftware.com>
2022-02-03 10:33:24 +0000

    yapf fix.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,9 @@
 import pytest
 
+
 # The following code allows custom flags when running pytest. In the future we could add --integrationtest and --unittest etc.
 def pytest_addoption(parser):
-    parser.addoption(
-        "--buildtest", action="store_true", default=False, help="run build tests"
-    )
+    parser.addoption("--buildtest", action = "store_true", default = False, help = "run build tests")
 
 
 def pytest_configure(config):
@@ -15,7 +14,7 @@ def pytest_collection_modifyitems(config, items):
     if config.getoption("--buildtest"):
         # --buildtest given in cli: do not skip build tests
         return
-    skip_slow = pytest.mark.skip(reason="need --buildtest option to run")
+    skip_slow = pytest.mark.skip(reason = "need --buildtest option to run")
     for item in items:
         if "buildtest" in item.keywords:
             item.add_marker(skip_slow)
