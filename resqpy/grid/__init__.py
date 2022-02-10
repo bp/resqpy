90fb4573e21891d4257d51f0461acc0b2bdcbff0
Max Maunder <max.maunder@bjss.com>
2022-02-10 09:30:16 +0000

    Module inits updated

--- resqpy/grid/__init__.py
+++ resqpy/grid/__init__.py
@@ -1,14 +1,19 @@
 """The Grid Module"""
 
 __all__ = [
-    'Grid', 'transmissibility', 'RegularGrid', 'extract_grid_parent', 'establish_zone_property_kind',
-    'find_cell_for_x_sect_xz', 'grid_flavour', 'is_regular_grid', 'any_grid'
+    'Grid', 'RegularGrid', 'extract_grid_parent', 'establish_zone_property_kind', 'find_cell_for_x_sect_xz',
+    'grid_flavour', 'is_regular_grid', 'any_grid'
 ]
 
-from .transmissibility import transmissibility
-from .grid import Grid
-from .regular_grid import RegularGrid
-from .grid_types import grid_flavour, is_regular_grid, any_grid
-from .moved_functions import establish_zone_property_kind
-from .extract_functions import extract_grid_parent, extent_kji_from_root
-from .points_functions import find_cell_for_x_sect_xz
+from ._grid import Grid
+from ._regular_grid import RegularGrid
+from ._grid_types import grid_flavour, is_regular_grid, any_grid
+from ._moved_functions import establish_zone_property_kind
+from ._extract_functions import extract_grid_parent, extent_kji_from_root
+from ._points_functions import find_cell_for_x_sect_xz
+
+# Set "module" attribute of all public objects to this path.
+for _name in __all__:
+    _obj = eval(_name)
+    if hasattr(_obj, "__module__"):
+        _obj.__module__ = __name__
