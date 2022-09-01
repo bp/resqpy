a4f0a5ee1bb232ae476a59f3e29f57772464390e
Max Maunder <max_maunder@hotmail.co.uk>
2022-09-01 13:53:53 +0100

    Jit decorator removed

--- resqpy/grid_surface.py
+++ resqpy/grid_surface.py
@@ -1610,7 +1610,6 @@ def intersect_numba(axis: int, index1: int, index2: int, hits: np.ndarray, n_axi
     return faces, normals, offsets, triangle_per_face
 
 
-@njit
 def bisector_from_faces(grid_extent_kji: Tuple[int, int, int], k_faces: np.ndarray, j_faces: np.ndarray,
                         i_faces: np.ndarray) -> Tuple[np.ndarray, bool]:
     """Returns a numpy bool array denoting the bisection of the grid by the face sets.
@@ -1633,8 +1632,8 @@ def bisector_from_faces(grid_extent_kji: Tuple[int, int, int], k_faces: np.ndarr
     assert len(grid_extent_kji) == 3
     # a = np.zeros(grid_extent_kji, dtype = numba.boolean)  # initialise to False
     # c = np.zeros(grid_extent_kji, dtype = numba.boolean)  # cells changing
-    a = np.zeros(grid_extent_kji, dtype = numba.boolean)  # initialise to False
-    c = np.zeros(grid_extent_kji, dtype = numba.boolean)  # cells changing
+    a = np.zeros(grid_extent_kji, dtype = np.bool_)  # initialise to False
+    c = np.zeros(grid_extent_kji, dtype = np.bool_)  # cells changing
     open_k = np.logical_not(k_faces)
     open_j = np.logical_not(j_faces)
     open_i = np.logical_not(i_faces)
