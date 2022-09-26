8abea5e53a2043399cc4bbb202e533034782eba0
Andy Beer <andy.beer@bp.com>
2022-09-26 17:52:19 +0100

    allowing hyphens in recurrent property names in vdb import

--- resqpy/rq_import/_import_nexus.py
+++ resqpy/rq_import/_import_nexus.py
@@ -522,8 +522,16 @@ def import_nexus(
                 # for each property for this timestep, cache array and add to recur prop import collection for this time step
                 if recur_prop_list:
                     for keyword in recur_prop_list:
-                        if not keyword or not keyword.isalnum():
+                        if not keyword:
                             continue
+                        if not keyword.isalnum():
+                            reject = False
+                            for c in keyword:
+                                if not c.isalnum() and c != '-':
+                                    reject = True
+                                    break
+                            if reject:
+                                continue
                         prop_kind, facet_type, facet = rp.property_kind_and_facet_from_keyword(keyword)
                         step_import_collection.import_vdb_recurrent_property_to_cache(
                             vdbase,
