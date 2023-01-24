0026d7d523dedce5f8e17a46262c6c4a91088a3d
Andy Beer <andy.beer@bp.com>
2023-01-24 21:07:55 +0530

    MyPy appeasement

--- resqpy/multiprocessing/multiprocessing.py
+++ resqpy/multiprocessing/multiprocessing.py
@@ -4,7 +4,7 @@ import logging
 import os
 import time
 import uuid
-import joblib
+import joblib  # type: ignore
 from typing import List, Dict, Any, Callable, Union
 from pathlib import Path
 from joblib import Parallel, delayed, parallel_backend  # type: ignore
