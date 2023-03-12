aa85e53ca65c0260824c1b48fcc6e200f5fb38dc
Andy Beer <andy.beer@bp.com>
2023-03-12 18:41:27 +0530

    pillar geometry defined tests added

--- tests/unit_tests/grid/test_defined_geometry.py
+++ tests/unit_tests/grid/test_defined_geometry.py
@@ -5,7 +5,7 @@ import resqpy.model as rq
 import resqpy.grid as grr
 
 
-def add_grid_with_missing_points(model, crs):
+def add_grid_with_missing_points(model, crs, missing_pillar = False):
     """Create an irregular grid with some points missing."""
     grid = grr.Grid(model, title = 'partial')
     grid.crs = crs
@@ -16,21 +16,28 @@ def add_grid_with_missing_points(model, crs):
     grid.k_direction_is_down = True
     grid.pillar_shape = 'straight'
     grid.has_split_coordinate_lines = False
+    # yapf: disable
     grid.points_cached = np.array(
-        [[[(100.0, 1000.0, 5000.0), (200.0, 1000.0, 5000.0),
-           (np.NaN, np.NaN, np.NaN)], [(100.0, 1200.0, 5000.0), (200.0, 1200.0, 5000.0), (300.0, 1200.0, 5000.0)],
+        [[[(100.0, 1000.0, 5000.0), (200.0, 1000.0, 5000.0), (np.NaN, np.NaN, np.NaN)],
+          [(100.0, 1200.0, 5000.0), (200.0, 1200.0, 5000.0), (300.0, 1200.0, 5000.0)],
           [(100.0, 1500.0, 5000.0), (200.0, 1500.0, 5000.0), (300.0, 1500.0, 5000.0)]],
-         [[(110.0, 1005.0, 5020.0), (210.0, 1005.0, 5020.0),
-           (310.0, 1005.0, 5020.0)], [(110.0, 1205.0, 5020.0), (210.0, 1205.0, 5020.0), (310.0, 1205.0, 5020.0)],
+         [[(110.0, 1005.0, 5020.0), (210.0, 1005.0, 5020.0), (310.0, 1005.0, 5020.0)],
+          [(110.0, 1205.0, 5020.0), (210.0, 1205.0, 5020.0), (310.0, 1205.0, 5020.0)],
           [(110.0, 1505.0, 5020.0), (210.0, 1505.0, 5020.0), (310.0, 1505.0, 5020.0)]],
-         [[(120.0, 1010.0, 5040.0), (220.0, 1010.0, 5040.0),
-           (320.0, 1010.0, 5040.0)], [(120.0, 1210.0, 5040.0), (220.0, 1210.0, 5040.0), (320.0, 1210.0, 5040.0)],
+         [[(120.0, 1010.0, 5040.0), (220.0, 1010.0, 5040.0), (320.0, 1010.0, 5040.0)],
+          [(120.0, 1210.0, 5040.0), (220.0, 1210.0, 5040.0), (320.0, 1210.0, 5040.0)],
           [(np.NaN, np.NaN, np.NaN), (220.0, 1510.0, 5040.0), (320.0, 1510.0, 5040.0)]]],
         dtype = float)
+    # yapf: enable
     assert grid.points_cached.shape == (3, 3, 3, 3)
     grid.array_cell_geometry_is_defined = np.ones((2, 2, 2), dtype = bool)
-    grid.array_cell_geometry_is_defined[0, 0, 1] = False
     grid.array_cell_geometry_is_defined[1, 1, 0] = False
+    grid.array_cell_geometry_is_defined[0, 0, 1] = False
+    if missing_pillar:
+        grid.points_cached[:, 1, 0, :] = np.NaN
+        grid.array_cell_geometry_is_defined[:, 1, 0] = False
+        grid.array_pillar_geometry_is_defined = np.ones((3, 3), dtype = bool)
+        grid.array_pillar_geometry_is_defined[1, 0] = False
     grid.write_hdf5_from_caches()
     grid.create_xml()
     return grid
@@ -57,4 +64,34 @@ def test_cell_geometry_is_defined_cell_kji0(example_model_and_crs):
     assert grid.cell_geometry_is_defined(cell_kji0 = (0, 0, 0), cache_array = False)
     assert not grid.cell_geometry_is_defined(cell_kji0 = (0, 0, 1), cache_array = False)
     assert not grid.cell_geometry_is_defined(cell_kji0 = (1, 1, 0), cache_array = False)
+    all_cells = grid.geometry_defined_for_all_cells_cached
+    assert all_cells is not None and not all_cells
+    assert grid.cell_geometry_is_defined(cell_kji0 = (1, 0, 1), cache_array = True)
+    assert np.count_nonzero(grid.array_cell_geometry_is_defined) == 6
+
+
+def test_pillar_geometry_is_defined_everywhere(example_model_and_crs):
+    model, crs = example_model_and_crs
+    add_grid_with_missing_points(model, crs)
+    model.store_epc()
+    model = rq.Model(model.epc_file)
+    grid = model.grid()
+    grid.cache_all_geometry_arrays()
+    assert grid.geometry_defined_for_all_pillars_cached
+    assert grid.pillar_geometry_is_defined(pillar_ji0 = (0, 1))
+    assert grid.pillar_geometry_is_defined(pillar_ji0 = (1, 0))
+
+
+def test_missing_pillar_geometry(example_model_and_crs):
+    model, crs = example_model_and_crs
+    add_grid_with_missing_points(model, crs, missing_pillar = True)
+    model.store_epc()
+    model = rq.Model(model.epc_file)
+    grid = model.grid()
+    grid.cache_all_geometry_arrays()
     assert not grid.geometry_defined_for_all_cells_cached
+    assert not grid.geometry_defined_for_all_pillars_cached
+    assert grid.pillar_geometry_is_defined(pillar_ji0 = (0, 0), cache_array = False)
+    assert grid.pillar_geometry_is_defined(pillar_ji0 = (1, 1), cache_array = True)
+    assert grid.array_pillar_geometry_is_defined[0, 1]
+    assert not grid.array_pillar_geometry_is_defined[1, 0]
