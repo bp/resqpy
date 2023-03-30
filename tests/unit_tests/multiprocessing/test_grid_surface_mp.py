0aa18221cd1079f6d2d257391e370bbe291ceb0e
Andy Beer <andy.beer@bp.com>
2023-03-30 16:03:05 +0530

    adding unit test for use_pack True

--- tests/unit_tests/multiprocessing/test_grid_surface_mp.py
+++ tests/unit_tests/multiprocessing/test_grid_surface_mp.py
@@ -2,6 +2,7 @@ from resqpy.model import Model
 from typing import Tuple
 from resqpy.grid import RegularGrid
 from resqpy.surface import Surface
+import resqpy.property as rqp
 from resqpy.multi_processing.wrappers.grid_surface_mp import find_faces_to_represent_surface_regular_wrapper
 from resqpy.multi_processing._multiprocessing import rm_tree
 import resqpy.olio.xml_et as rqet
@@ -171,3 +172,51 @@ def test_find_faces_to_represent_surface_regular_wrapper_properties_flange(small
     assert len(model.uuids(obj_type = 'ContinuousProperty')) == 4
     assert len(model.uuids()) == 16
     assert len(uuid_list) == 10
+
+
+def test_find_faces_to_represent_surface_extended_bisector_use_pack(small_grid_and_extended_surface: Tuple[RegularGrid,
+                                                                                                           Surface]):
+    # Arrange
+    grid, surface = small_grid_and_extended_surface
+    grid_epc = surface_epc = grid.model.epc_file
+    grid_uuid = grid.uuid
+    surface_uuid = surface.uuid
+    return_properties = ["triangle", "offset", "grid bisector", "grid shadow"]
+
+    name = "test"
+    input_index = 0
+    use_index_as_realisation = False
+
+    # Act
+    index, success, epc_file, uuid_list = find_faces_to_represent_surface_regular_wrapper(
+        input_index,
+        "tmp_dir",
+        use_index_as_realisation,
+        grid_epc,
+        grid_uuid,
+        surface_epc,
+        surface_uuid,
+        name,
+        return_properties = return_properties,
+        use_pack = True)
+    model = Model(epc_file = epc_file)
+
+    # Assert
+    assert success is True
+    assert index == input_index
+    assert len(model.uuids(obj_type = 'LocalDepth3dCrs')) == 1
+    assert len(model.uuids(obj_type = 'IjkGridRepresentation')) == 1
+    assert len(model.uuids(obj_type = 'TriangulatedSetRepresentation')) == 2
+    assert len(model.uuids(obj_type = 'GridConnectionSetRepresentation')) == 1
+    assert len(model.uuids(obj_type = 'FaultInterpretation')) == 1
+    assert len(model.uuids(obj_type = 'TectonicBoundaryFeature')) == 1
+    assert len(model.uuids(obj_type = 'DiscreteProperty')) == 3
+    assert len(model.uuids(obj_type = 'ContinuousProperty')) == 4
+    assert len(model.uuids()) == 18
+    assert len(uuid_list) == 11
+
+    for uuid in model.uuids(obj_type = 'DiscreteProperty'):
+        a = rqp.Property(model, uuid = uuid).array_ref()
+        assert a is not None
+
+    rm_tree("tmp_dir")
