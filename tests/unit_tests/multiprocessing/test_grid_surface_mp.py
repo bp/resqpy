b570c37d5ab6336bf0d7eb9ce0bf0ebb909c903a
Andy Beer <andy.beer@bp.com>
2023-08-01 14:18:06 +0300

    adding unit test for random agitation

--- tests/unit_tests/multiprocessing/test_grid_surface_mp.py
+++ tests/unit_tests/multiprocessing/test_grid_surface_mp.py
@@ -29,6 +29,46 @@ def test_find_faces_to_represent_surface_regular_wrapper(small_grid_and_surface:
                                                                                           surface_epc,
                                                                                           surface_uuid,
                                                                                           name,
+                                                                                          random_agitation = False,
+                                                                                          trimmed = True)
+    model = Model(epc_file = epc_file)
+    rm_tree("tmp_dir")
+
+    # Assert
+    assert success is True
+    assert index == input_index
+    assert len(model.uuids(obj_type = 'LocalDepth3dCrs')) == 1
+    assert len(model.uuids(obj_type = 'IjkGridRepresentation')) == 1
+    assert len(model.uuids(obj_type = 'TriangulatedSetRepresentation')) == 1
+    assert len(model.uuids(obj_type = 'GridConnectionSetRepresentation')) == 1
+    assert len(model.uuids(obj_type = 'FaultInterpretation')) == 1
+    assert len(model.uuids(obj_type = 'TectonicBoundaryFeature')) == 1
+    assert len(model.uuids()) == 9
+    assert len(uuid_list) == 7
+
+
+def test_find_faces_to_represent_surface_regular_wrapper_random_agitation(small_grid_and_surface: Tuple[RegularGrid,
+                                                                                                        Surface]):
+    # Arrange
+    grid, surface = small_grid_and_surface
+    grid_epc = surface_epc = grid.model.epc_file
+    grid_uuid = grid.uuid
+    surface_uuid = surface.uuid
+
+    name = "test"
+    input_index = 0
+    use_index_as_realisation = False
+
+    # Act
+    index, success, epc_file, uuid_list = find_faces_to_represent_surface_regular_wrapper(input_index,
+                                                                                          "tmp_dir",
+                                                                                          use_index_as_realisation,
+                                                                                          grid_epc,
+                                                                                          grid_uuid,
+                                                                                          surface_epc,
+                                                                                          surface_uuid,
+                                                                                          name,
+                                                                                          random_agitation = True,
                                                                                           trimmed = True)
     model = Model(epc_file = epc_file)
     rm_tree("tmp_dir")
