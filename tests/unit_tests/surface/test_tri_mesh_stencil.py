005bda6d14903c2e06223deb0dd6faf8e10e64ad
Andy Beer <andy.beer@bp.com>
2024-02-25 20:13:35 -0600

    further unit tests of some class methods

--- tests/unit_tests/surface/test_tri_mesh_stencil.py
+++ tests/unit_tests/surface/test_tri_mesh_stencil.py
@@ -62,3 +62,58 @@ def test_tri_mesh_stencil_init_normalize_non_flat():
         np.array([(25.0, 250.0, 900.0, 250.0, 25.0), (25.0, 250.0, 250.0, 25.0, np.nan),
                   (25.0, 25.0, 25.0, np.nan, np.nan)],
                  dtype = float) / 27.0)
+
+
+def test_stencil_for_constant_unnormalized():
+    stencil = rqs.TriMeshStencil.for_constant_unnormalized(4, 7.0)
+    assert stencil.n == 4
+    assert stencil.pattern.shape == (4,) and np.all(np.isclose(stencil.pattern, 7.0))
+    assert np.all(stencil.start_ip == (-3, -3, -2, -2))
+    assert np.all(stencil.row_length == (7, 6, 5, 4))
+    assert np.isclose(np.nanmin(stencil.half_hex), 7.0)
+    assert np.isclose(np.nanmax(stencil.half_hex), 7.0)
+
+
+def test_stencil_for_constant_normalized_flat():
+    stencil = rqs.TriMeshStencil.for_constant_normalized(5, normalize_mode_flat = True)
+    assert stencil.n == 5
+    assert np.all(np.isclose(stencil.pattern, 1.0 / 61))
+    assert np.all(stencil.start_ip == (-4, -4, -3, -3, -2))
+    assert np.all(stencil.row_length == (9, 8, 7, 6, 5))
+    assert np.count_nonzero(np.isnan(stencil.half_hex)) == 10
+    assert np.isclose(np.nanmin(stencil.half_hex), 1.0 / 61)
+    assert np.isclose(np.nanmax(stencil.half_hex), 1.0 / 61)
+
+
+def test_stencil_for_constant_normalized_non_flat():
+    stencil = rqs.TriMeshStencil.for_constant_normalized(5, normalize_mode_flat = False)
+    assert stencil.n == 5
+    assert_array_almost_equal(stencil.pattern, (1.0 / 5, 1.0 / 30.0, 1.0 / 60, 1.0 / 90, 1.0 / 120))
+    assert np.all(stencil.start_ip == (-4, -4, -3, -3, -2))
+    assert np.all(stencil.row_length == (9, 8, 7, 6, 5))
+    assert np.count_nonzero(np.isnan(stencil.half_hex)) == 10
+    assert np.isclose(np.nanmin(stencil.half_hex), 1.0 / 120)
+    assert np.isclose(np.nanmax(stencil.half_hex), 1.0 / 5)
+
+
+def test_stencil_for_linear_unnormalized():
+    stencil = rqs.TriMeshStencil.for_linear_unnormalized(3, 5.0, 1.0)
+    assert stencil.n == 3
+    assert_array_almost_equal(stencil.pattern, (5.0, 3.0, 1.0))
+    assert np.all(stencil.start_ip == (-2, -2, -1))
+    assert np.all(stencil.row_length == (5, 4, 3))
+    assert_array_almost_equal(
+        stencil.half_hex, np.array([(1, 3, 5, 3, 1), (1, 3, 3, 1, np.nan), (1, 1, 1, np.nan, np.nan)], dtype = float))
+
+
+def test_stencil_for_linear_normalized_flat():
+    stencil = rqs.TriMeshStencil.for_linear_normalized(4, normalize_mode_flat = True)
+    assert stencil.n == 4
+    assert_array_almost_equal(stencil.pattern, np.array((4, 3, 2, 1), dtype = float) / 64.0)
+    assert np.all(stencil.start_ip == (-3, -3, -2, -2))
+    assert np.all(stencil.row_length == (7, 6, 5, 4))
+    assert_array_almost_equal(
+        stencil.half_hex, 3.0 / (16.0 * np.array([(12, 6, 4, 3, 4, 6, 12), (12, 6, 4, 4, 6, 12, np.nan),
+                                                  (12, 6, 6, 6, 12, np.nan, np.nan),
+                                                  (12, 12, 12, 12, np.nan, np.nan, np.nan)],
+                                                 dtype = float)))
