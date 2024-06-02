9681cb3af29f3dcc23c1ed12b3924c755d99f41c
Andy Beer <andy.beer@bp.com>
2024-06-02 21:03:40 +0100

    property subpackage init module update

--- resqpy/property/__init__.py
+++ resqpy/property/__init__.py
@@ -1,8 +1,8 @@
 """Collections of properties for grids, wellbore frames, grid connection sets etc."""
 
 __all__ = [
-    'PropertyCollection', 'Property', 'WellLog', 'WellIntervalProperty', 'WellIntervalPropertyCollection',
-    'WellLogCollection', 'StringLookup', 'PropertyKind', 'GridPropertyCollection',
+    'PropertyCollection', 'Property', 'AttributePropertySet', 'WellLog', 'WellIntervalProperty',
+    'WellIntervalPropertyCollection', 'WellLogCollection', 'StringLookup', 'PropertyKind', 'GridPropertyCollection',
     'property_over_time_series_from_collection', 'property_collection_for_keyword', 'infer_property_kind',
     'write_hdf5_and_create_xml_for_active_property', 'reformat_column_edges_to_resqml_format',
     'reformat_column_edges_from_resqml_format', 'same_property_kind', 'selective_version_of_collection',
@@ -30,6 +30,7 @@ from .property_common import property_collection_for_keyword,  \
 from .property_kind import PropertyKind, create_transmisibility_multiplier_property_kind
 from .string_lookup import StringLookup
 from .property_collection import PropertyCollection
+from .attribute_property_set import AttributePropertySet
 from .grid_property_collection import GridPropertyCollection
 from ._property import Property
 from .well_interval_property import WellIntervalProperty
