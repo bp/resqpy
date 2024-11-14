53111ac7226f213d561eabe76d3410bab59ac1a4
MahmoudAlamar03 <hamadaalzain88@gmail.com>
2024-11-14 07:27:49 -0600

    Updating deprecation warning.

--- resqpy/olio/read_nexus_fault.py
+++ resqpy/olio/read_nexus_fault.py
@@ -64,7 +64,8 @@ def load_nexus_fault_mult_table_from_list(file_as_list):
                         outdata[mask] = np.concatenate(d_elems)
                         df = pd.DataFrame(outdata)
                         for column in df.columns:
-                            df[column] = pd.to_numeric(df[column], errors = 'ignore')
+                            df[column] = pd.to_numeric(df[column])
+                            
                         df.columns = ['i1', 'i2', 'j1', 'j2', 'k1', 'k2', 'mult']
                         df['grid'] = grid
                         df['name'] = name
