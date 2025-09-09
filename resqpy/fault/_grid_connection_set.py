32834df5c8b282bb8713c2c149ae25b4cbb19417
emmanesbit <84792288+emmanesbit@users.noreply.github.com>
2025-09-09 15:23:00 +0100

    Update _grid_connection_set.py

--- resqpy/fault/_grid_connection_set.py
+++ resqpy/fault/_grid_connection_set.py
@@ -2303,7 +2303,7 @@ def _copy_organisation_objects(target_model, source_model, gcs):
                                                 uuid)  # will copy related features as well as interpretations
 
 
-def _sort_and_remove_duplicates(a: np.ndarray, props: Optional[list] = None):
+def _sort_and_remove_duplicates(a: np.ndarray, props: Optional[List[np.ndarray]] = None):
     """Return copy of 1D array a, sorted and with duplicates removed; secondary arrays can be kept in alignment. 
     
     note: If present, property arrays are also sorted in-place with duplicates removed.
@@ -2322,6 +2322,7 @@ def _sort_and_remove_duplicates(a: np.ndarray, props: Optional[list] = None):
     m[0] = True
     m[1:] = (a[1:] != a[:-1])
     if not no_props:
+        assert props is None
         for i in range(len(props)):
             p = props[i][si]
             props[i] = p[m]
